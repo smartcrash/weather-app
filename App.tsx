866e7e6c8c8b46783f513dffdf2ce4e5f274cac8
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-31 11:56:42 -0400

    Update design

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
-import { FontAwesome } from "@expo/vector-icons";
 import { format } from "date-fns";
+import { LinearGradient } from "expo-linear-gradient";
 import {
   Box,
   Center,
+  Divider,
+  extendTheme,
   Heading,
   HStack,
-  Icon,
   NativeBaseProvider,
   Text,
   VStack,
 } from "native-base";
 import useLocation from "./hooks/useLocation";
 import useWeather from "./hooks/useWeather";
+
+const theme = extendTheme({ config: { initialColorMode: "dark" } });
 
 const WEATHER_ICONS: Record<string, string> = {
   "clear sky": "☀️",
@@ -40,81 +43,114 @@ export default function App() {
   const details = [
     {
       key: "Wind",
-      value: `${data.wind.speed.toFixed(0)} m/s`,
+      value: `${data.wind.speed.toFixed(0)}`,
+      unit: "m/s",
     },
     {
       key: "Humidity",
-      value: `${data.main.humidity.toFixed(0)}%`,
+      value: `${data.main.humidity.toFixed(0)}`,
+      unit: "%",
     },
     {
       key: "Feels like",
-      value: `${data.main.feels_like.toFixed(0)}°`,
+      value: `${data.main.feels_like.toFixed(0)}`,
+      unit: "°",
     },
   ];
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
+      <LinearGradient
+        colors={["#5bd6f7", "#71b5fa", "#1069f4"]}
+        start={{ x: 0.8, y: 0 }}
+        end={{ x: 0, y: 0.9 }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
       <VStack
         safeArea
         height={"full"}
         justifyContent={"center"}
         alignItems={"center"}
-        paddingX={9}
+        paddingX={6}
         paddingTop={6}
-        paddingBottom={6}
+        paddingBottom={8}
       >
-        <HStack
-          space={1}
-          alignItems={"center"}
-          flexShrink={0}
-          borderWidth={1}
-          borderRadius={"full"}
-          paddingX={4}
-          paddingY={0.5}
-        >
-          <Icon
-            as={FontAwesome}
-            name={"map-marker"}
-            color={"black"}
-            size={"sm"}
-          />
-          <Text fontSize={"lg"}>{data.name}</Text>
-        </HStack>
+        <VStack width={"full"} flexShrink={0}>
+          <Heading fontSize={"2xl"} fontWeight={"semibold"}>
+            {data.name}
+          </Heading>
+          <Text fontSize={"sm"} opacity={0.75}>
+            {format(new Date(), "eeee, d LLL")}
+          </Text>
+        </VStack>
 
         <Box flexGrow={1}>
           <Center marginY={"auto"}>
+            {/*
+            TODO: Change icons
             <Text fontSize={"180px"}>
               {WEATHER_ICONS[data.weather[0].description]}
             </Text>
+            */}
             <HStack>
-              <Heading fontWeight={"extrabold"} fontSize={"9xl"}>
+              <Heading
+                fontWeight={"normal"}
+                // fontSize={"9xl"}
+                fontSize={"150px"}
+                style={{
+                  textShadowColor: "white",
+                  textShadowOffset: { width: 0, height: 0 },
+                  textShadowRadius: 9,
+                }}
+              >
                 {data.main.temp.toFixed(0)}
               </Heading>
-              <Text fontSize={"6xl"} marginTop={2} color={"gray.300"}>
+              <Text fontSize={"6xl"} marginTop={2} opacity={0.5}>
                 °
               </Text>
             </HStack>
-            <Text fontSize={"3xl"} fontWeight={"bold"}>
+            <Text fontSize={"3xl"} fontWeight={"normal"}>
               {data.weather[0].main}
             </Text>
-            <Text marginTop={2} fontSize={"sm"} color={"gray.400"}>
-              {format(new Date(), "eeee, d LLL")}
+            <Text fontSize={"md"} opacity={0.5}>
+              {data.weather[0].description}
             </Text>
           </Center>
         </Box>
 
+        <Divider background={"white"} marginY={4} opacity={0.2} />
+
         <HStack justifyContent={"space-between"} flexShrink={0} width={"full"}>
-          {details.map(({ key, value }, index) => (
+          {details.map(({ key, value, unit }, index) => (
             <VStack
               space={0.5}
               justifyContent={"center"}
               alignItems={"center"}
               key={index}
             >
-              <Text fontSize={"xl"} fontWeight={"bold"}>
-                {value}
-              </Text>
-              <Text color={"gray.400"} fontSize={"sm"}>
+              <HStack alignItems={"flex-start"} space={0}>
+                <Text
+                  fontSize={"4xl"}
+                  fontWeight={"semibold"}
+                  style={{
+                    textShadowColor: "white",
+                    textShadowOffset: { width: 0, height: 0 },
+                    textShadowRadius: 5,
+                  }}
+                >
+                  {value}
+                </Text>
+                <Text fontSize={"md"} fontWeight={"bold"} marginTop={2}>
+                  {unit}
+                </Text>
+              </HStack>
+              <Text fontSize={"sm"} opacity={0.75}>
                 {key}
               </Text>
             </VStack>
