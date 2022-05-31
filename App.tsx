e00078f00ec0fe61ea04865db6043cfbcdc431e1
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-31 11:00:30 -0400

    Update App.tsx content

--- App.tsx
+++ App.tsx
@@ -1,79 +1,120 @@
-import { FontAwesome } from '@expo/vector-icons'
-import { Box, Center, Heading, HStack, Icon, NativeBaseProvider, Text, VStack } from 'native-base'
+import { FontAwesome } from "@expo/vector-icons";
+import { format } from "date-fns";
+import {
+  Box,
+  Center,
+  Heading,
+  HStack,
+  Icon,
+  NativeBaseProvider,
+  Text,
+  VStack,
+} from "native-base";
+import useLocation from "./hooks/useLocation";
+import useWeather from "./hooks/useWeather";
 
-const API_KEY = '934ed876a8de43bf010a96d95a05b30c'
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
+const WEATHER_ICONS: Record<string, string> = {
+  "clear sky": "☀️",
+  "few clouds": "🌤",
+  "scattered clouds": "☁️",
+  "broken clouds": "🌤",
+  "shower rain": "🌧",
+  rain: "🌧",
+  thunderstorm: "⛈",
+  snow: "🌨",
+  mist: "🌫",
+};
 
 export default function App() {
+  const [location, { loading, error }] = useLocation();
+  const [data] = useWeather(
+    location?.coords.latitude,
+    location?.coords.longitude
+  );
+
+  // TODO: Handle loading state
+  if (!data) {
+    return <></>;
+  }
+
   const details = [
     {
-      key: 'Wind',
-      value: '12 km/h',
-      icon: '💨',
+      key: "Wind",
+      value: `${data.wind.speed.toFixed(0)} m/s`,
     },
     {
-      key: 'Humidity',
-      value: '63%',
-      icon: '💧',
+      key: "Humidity",
+      value: `${data.main.humidity.toFixed(0)}%`,
     },
     {
-      key: 'Feels like',
-      value: '28°',
-      icon: '🌡️',
+      key: "Feels like",
+      value: `${data.main.feels_like.toFixed(0)}°`,
     },
-  ]
+  ];
 
   return (
     <NativeBaseProvider>
       <VStack
         safeArea
-        height={'full'}
-        justifyContent={'center'}
-        alignItems={'center'}
+        height={"full"}
+        justifyContent={"center"}
+        alignItems={"center"}
         paddingX={9}
         paddingTop={6}
         paddingBottom={6}
       >
         <HStack
           space={1}
-          alignItems={'center'}
+          alignItems={"center"}
           flexShrink={0}
           borderWidth={1}
-          borderRadius={'full'}
+          borderRadius={"full"}
           paddingX={4}
           paddingY={0.5}
         >
-          <Icon as={FontAwesome} name={'map-marker'} color={'black'} size={'sm'} />
-          <Text fontSize={'lg'}>Valencia</Text>
+          <Icon
+            as={FontAwesome}
+            name={"map-marker"}
+            color={"black"}
+            size={"sm"}
+          />
+          <Text fontSize={"lg"}>{data.name}</Text>
         </HStack>
 
         <Box flexGrow={1}>
-          <Center marginY={'auto'}>
-            <Text fontSize={'180px'}>⛅</Text>
+          <Center marginY={"auto"}>
+            <Text fontSize={"180px"}>
+              {WEATHER_ICONS[data.weather[0].description]}
+            </Text>
             <HStack>
-              <Heading fontWeight={'extrabold'} fontSize={'9xl'}>
-                30
+              <Heading fontWeight={"extrabold"} fontSize={"9xl"}>
+                {data.main.temp.toFixed(0)}
               </Heading>
-              <Text fontSize={'6xl'} marginTop={2} color={'gray.300'}>
+              <Text fontSize={"6xl"} marginTop={2} color={"gray.300"}>
                 °
               </Text>
             </HStack>
-            <Text fontSize={'3xl'} fontWeight={'bold'}>
-              Sunny
+            <Text fontSize={"3xl"} fontWeight={"bold"}>
+              {data.weather[0].main}
             </Text>
-            <Text marginTop={2} fontSize={'sm'} color={'gray.400'}>
-              Monday, 17 May
+            <Text marginTop={2} fontSize={"sm"} color={"gray.400"}>
+              {format(new Date(), "eeee, d LLL")}
             </Text>
           </Center>
         </Box>
 
-        <HStack justifyContent={'space-between'} flexShrink={0} width={'full'}>
-          {details.map(({ key, value, icon }, index) => (
-            <VStack space={0.5} justifyContent={'center'} alignItems={'center'} key={index}>
-              <Text fontSize={'xl'} fontWeight={'bold'}>
+        <HStack justifyContent={"space-between"} flexShrink={0} width={"full"}>
+          {details.map(({ key, value }, index) => (
+            <VStack
+              space={0.5}
+              justifyContent={"center"}
+              alignItems={"center"}
+              key={index}
+            >
+              <Text fontSize={"xl"} fontWeight={"bold"}>
                 {value}
               </Text>
-              <Text color={'gray.400'} fontSize={'sm'}>
+              <Text color={"gray.400"} fontSize={"sm"}>
                 {key}
               </Text>
             </VStack>
@@ -81,5 +122,5 @@ export default function App() {
         </HStack>
       </VStack>
     </NativeBaseProvider>
-  )
+  );
 }
